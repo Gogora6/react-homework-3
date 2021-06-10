8b98848b70604ecfcd7421782a06cfbfd63976d6
gogora6 <lasha.gogorishvili.1@btu.edu.ge>
2021-06-10 18:32:29 +0400

    Fixed bugs

--- src/pages/auth/SignUp.jsx
+++ src/pages/auth/SignUp.jsx
@@ -1,10 +1,10 @@
-import SignUnForm from '../../components/forms/SignUpForm';
+import SignUpForm from '../../components/forms/SignUpForm';
 import { withNoAuth } from '../../hoc/withNoAuth';
 
 function SignUp(props) {
   return (
     <div className="container col-6 mt-5">
-      <SignUnForm />
+      <SignUpForm />
     </div>
   );
 }
